a9476e570c8419d413b0f0a8348f55bc6b6c53a1
pietro <pietrof@live.com>
2019-11-12 23:22:52 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,31 @@
-var myMap;
-var canvas;
-var mappa = new Mappa('MapboxGL', "pk.eyJ1IjoicGlldHJvZm9yaW5vIiwiYSI6ImNqeHgzd3JwajBrc2YzaXBma3UxODdmdWUifQ.8jG3b2D80IsptKlqlr0l8w");
+let myMap;
+let canvas;
+const mappa = new Mappa('MapboxGL', "pk.eyJ1IjoicGlldHJvZm9yaW5vIiwiYSI6ImNqeHgzd3JwajBrc2YzaXBma3UxODdmdWUifQ.8jG3b2D80IsptKlqlr0l8w");
 
-var myLat;
-var myLon;
+var duomoLat = 45.4641013;
+var duomoLon = 9.1897325;
 
-var position;
-
-function preload(){
-  position = getCurrentPosition();
-}
-
-var options = {
-  lat: 0,
-  lng: 0,
+// Lets put all our map options in a single object
+const options = {
+  lat: duomoLat,
+  lng: duomoLon,
   zoom: 13,
-  style: 'mapbox://styles/pietroforino/ck2wdg4id1ivd1co69u1t7nys'
+  style: "mapbox://styles/pietroforino/ck2wdg4id1ivd1co69u1t7nys" // default tile for Leaflet
 }
 
-function setup() {
-  console.log(position);
-
+function setup(){
   canvas = createCanvas(windowWidth,windowHeight);
 
-  myLat = position.latitude;
-  myLon = position.longitude;
-
-  options.lat = myLat;
-  options.lng = myLon;
-
-  myMap = mappa.tileMap(options); //dentro a tileMap mettiamo tutte le opzioni create
+  // Create a tile map with the options declared
+  myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
 }
 
-function draw() {
-  clear(); //delete all the contect that is inside our canvas
+function draw(){
+  clear();
 
-  var myPosition = myMap.latLngToPixel(myLat, myLon); //funzione che trasforma le cordinate lat e lon in x e y coordinate
-
-  fill('#AFFC41');
+  var point = myMap.latLngToPixel(duomoLat, duomoLon);
+  fill(255, 0, 0);
   noStroke();
-
-  ellipse(myPosition.x, myPosition.y, 25);
+  ellipse(point.x, point.y, 10)
 }
